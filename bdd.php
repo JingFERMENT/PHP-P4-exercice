0da876b1fbddd60bde571ca538c0ee3105d7d6c0
Jing ZHANG <jing.zhang.ferment@gmail.com>
2024-10-18 14:48:29 +0200

    update a detailed page of a work of art

--- bdd.php
+++ bdd.php
@@ -1,22 +1,7 @@
 <?php
 
-require 'config.php';
+require_once 'config.php';
 
-try {
-    // Utiliser les constantes définies pour créer une connexion PDO
-    $pdo = new PDO(DSN, LOGIN, PASSWORD);
-
-    // Définir le mode d'erreur PDO pour lever des exceptions
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    // echo "Connexion réussie !";
-} catch (PDOException $e) {
-    // Gérer les erreurs de connexion
-    echo "Erreur de connexion : " . $e->getMessage();
-}
-
-$sqlQuery = 'SELECT * FROM oeuvres';
-$oeuvresStatement = $pdo->prepare($sqlQuery);
-$oeuvresStatement->execute();
-$oeuvres = $oeuvresStatement->fetchAll();
-
-// var_dump($oeuvres);
+function connexion(){
+    return new PDO(DSN, LOGIN, PASSWORD);
+}   
--- index.php
+++ index.php
@@ -1,9 +1,17 @@
 <?php
-    require 'header.php';
-    require 'bdd.php';
+require_once 'header.php';
+require_once 'bdd.php';
+
+    $pdo = connexion();
+    $sql = 'SELECT * FROM oeuvres';
+    $sth = $pdo->prepare($sql);
+    $sth->execute();
+    $oeuvres = $sth->fetchAll();
 ?>
+
+
 <div id="liste-oeuvres">
-    <?php foreach($oeuvres as $oeuvre): ?>
+    <?php foreach ($oeuvres as $oeuvre): ?>
         <article class="oeuvre">
             <a href="oeuvre.php?id=<?= $oeuvre['id'] ?>">
                 <img src="<?= $oeuvre['image'] ?>" alt="<?= $oeuvre['titre'] ?>">
